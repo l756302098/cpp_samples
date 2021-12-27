2a07bb6e5e77fbadbc72abdc78552e2a741a95fc
li <liyanlong@indemind.cn>
2021-12-27 10:53:31 +0800

    add merge

--- FileMerge.cpp
+++ FileMerge.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
+#include "FilePath.h"
 
 void SplitFile(){
 	//split image into 10 parts
@@ -69,6 +70,41 @@ void MergeFile(){
 	fwrite(buf, total, 1, fp);
 	fclose(fp);
 	delete buf;
+}
+
+void MergeFile2(){
+	//get total size
+	std::string totalBuf;
+	size_t totalSize = 0;
+	std::string rootPath = "/home/li/Documents/Test/";
+	for (size_t i = 0; i < 10; i++)
+	{
+		std::string childPath = rootPath + std::to_string(i);
+		size_t fileSize = swr::util::FilePath::GetFileSize(childPath.c_str());
+		if(fileSize != -1)
+		{
+			totalSize += fileSize;
+		}
+		char *buf = new char[fileSize];
+    	memset(buf,0x0,fileSize);
+		FILE *cfp;
+		/* 打开文件用于读写 */
+		cfp = fopen(childPath.c_str(), "r+");
+		/* 写入数据到文件 */
+		fread(buf, fileSize, 1, cfp);
+		fclose(cfp);
+		std::string str;
+		str.assign(&buf[0],&buf[strlen(buf)]);
+		totalBuf += str;
+		delete buf;
+	}
+	FILE *fp;
+	std::string targetPath = rootPath + "ori.jpg";
+	/* 打开文件用于读写 */
+	fp = fopen(targetPath.c_str(), "w+");
+	/* 写入数据到文件 */
+	fwrite(totalBuf.c_str(), totalSize, 1, fp);
+	fclose(fp);
 }
 
 int main(int argc, char *argv[])
