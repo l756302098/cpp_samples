17ac47f97d18356e8dfd05c0a2f0a9bebeb945c3
li <1976954875@qq.com>
2024-10-17 14:59:14 +0800

    string_view

---
+++ cppreference/string_view.cpp
@@ -0,0 +1,81 @@
+// basic_string_view_compare.cpp
+// compile with: /EHsc
+#include <string_view>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string to_alpha(int result)
+{
+   if (result < 0) return " less than ";
+   else if (result == 0) return " equal to ";
+   else return " greater than ";
+}
+
+int main()
+{
+   // The first member function compares
+   // two string_views
+   string_view sv_A("CAB");
+   string_view sv_B("CAB");
+   cout << "sv_A is " << sv_A << endl;
+   cout << "sv_B is " << sv_B << endl;
+   int comp1 = sv_A.compare(sv_B);
+   cout << "sv_A is" << to_alpha(comp1) << "sv_B.\n";
+
+   // The second member function compares part of
+   // an operand string_view to another string_view
+   string_view sv_C("AACAB");
+   string_view sv_D("CAB");
+   cout << "sv_C is: " << sv_C << endl;
+   cout << "sv_D is: " << sv_D << endl;
+   int comp2a = sv_C.compare(2, 3, sv_D);
+   cout << "The last three characters of sv_C are"
+       << to_alpha(comp2a) << "sv_D.\n";
+
+   int comp2b = sv_C.compare(0, 3, sv_D);
+   cout << "The first three characters of sv_C are"
+       << to_alpha(comp2b) << "sv_D.\n";
+
+   // The third member function compares part of
+   // an operand string_view to part of another string_view
+   string_view sv_E("AACAB");
+   string_view sv_F("DCABD");
+   cout << "sv_E: " << sv_E << endl;
+   cout << "sv_F is: " << sv_F << endl;
+   int comp3a = sv_E.compare(2, 3, sv_F, 1, 3);
+   cout << "The three characters from position 2 of sv_E are"
+       << to_alpha(comp3a)
+       << "the 3 characters of sv_F from position 1.\n";
+
+   // The fourth member function compares
+   // an operand string_view to a C string
+   string_view sv_G("ABC");
+   const char* cs_A = "DEF";
+   cout << "sv_G is: " << sv_G << endl;
+   cout << "cs_A is: " << cs_A << endl;
+   int comp4a = sv_G.compare(cs_A);
+   cout << "sv_G is" << to_alpha(comp4a) << "cs_A.\n";
+
+   // The fifth member function compares part of
+   // an operand string_view to a C string
+   string_view sv_H("AACAB");
+   const char* cs_B = "CAB";
+   cout << "sv_H is: " << sv_H << endl;
+   cout << "cs_B is: " << cs_B << endl;
+   int comp5a = sv_H.compare(2, 3, cs_B);
+   cout << "The last three characters of sv_H are"
+      << to_alpha(comp5a) << "cs_B.\n";
+
+   // The sixth member function compares part of
+   // an operand string_view to part of an equal length of
+   // a C string
+   string_view sv_I("AACAB");
+   const char* cs_C = "ACAB";
+   cout << "sv_I is: " << sv_I << endl;
+   cout << "cs_C: " << cs_C << endl;
+   int comp6a = sv_I.compare(1, 3, cs_C, 3);
+   cout << "The 3 characters from position 1 of sv_I are"
+      << to_alpha(comp6a) << "the first 3 characters of cs_C.\n";
+}
